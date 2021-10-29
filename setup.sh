22cbdba49bbc5899cc438a8b70d9e397bcb85cdf
Lexterd <79321771+sentinm@users.noreply.github.com>
2021-10-29 23:20:43 +0530

    Based on [Commit](https://github.com/mohitjoshi155/HerokuShell-Rclone/blob/master/setup.sh)

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install rclone static binary
+wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip -qqj rclone-current-linux-amd64.zip
+rm -rf rclone-current-linux-amd64.zip
+export PATH=$PWD/:$PATH
