469904d1b0a03f57bfb407c881de3fa217d0c7c4
Lexterd <79321771+sentinm@users.noreply.github.com>
2021-10-29 13:38:02 +0530

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,1 +1,1 @@
-export PORT=5000 & rcd --rc-web-gui --rc-addr=localhost:$PORT --rc-user=admin --rc-pass=sudo
+export PORT=5000 & rclone rcd --rc-web-gui --rc-addr=localhost:$PORT --rc-user=admin --rc-pass=sudo
