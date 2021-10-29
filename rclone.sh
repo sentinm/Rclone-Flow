edc95d97e00d3bb53e8356d835e47f7f8f341375
Lexterd <79321771+sentinm@users.noreply.github.com>
2021-10-29 09:32:34 +0530

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,1 +1,1 @@
-curl https://rclone.org/install.sh | sudo bash & rclone rcd --rc-web-gui --rc-user=admin --rc-pass=sudo
+curl https://rclone.org/install.sh | sudo bash & rclone rcd --rc-web-gui --rc-addr=localhost:$PORT --rc-user=admin --rc-pass=sudo
