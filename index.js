3566b4f3fa7fad7fc420df40715a5790dd4ba645
Lexterd <79321771+sentinm@users.noreply.github.com>
2021-10-29 19:00:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { exec } = require("child_process");
 const PORT = process.env.PORT || 6745;
 
+exec("curl https://rclone.org/install.sh | sudo bash", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`[Error] ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`[Std Error] ${stderr}`);
+        return;
+    }
+    console.log(`${stdout}`);
+});
+
 exec(`rcd --rc-web-gui --rc-addr=localhost:${PORT} --rc-user=sudo --rc-pass=sudo`, (error, stdout, stderr) => {
     if (error) {
         console.log(`[Error] ${error.message}`);
