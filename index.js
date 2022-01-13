e8714e2b3e7f5a94262ac95cd5dac7d9a0d5f590
Lexterd <79321771+sentinm@users.noreply.github.com>
2022-01-12 23:27:42 -0500

    Configuration
    Added configuration for PASSWORD and USERNAME envs.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { exec } = require("child_process");
 const PORT = process.env.PORT || 5000;
+const USERNAME = process.env.USERNAME || sudo;
+const PASSWORD = process.env.PASSWORD || sudo;
 
-exec(`rclone rcd --rc-web-gui --rc-addr=0.0.0.0:${ PORT } --rc-user=sudo --rc-pass=sudo`, (error, stdout, stderr) => {
+exec(`rclone rcd --rc-web-gui --rc-addr=0.0.0.0:${ PORT } --rc-user=${ USERNAME } --rc-pass=${ PASSWORD }`, (error, stdout, stderr) => {
     if (error) {
         console.log(`[Error] ${error.message}`);
         return;
