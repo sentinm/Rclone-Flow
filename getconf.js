1748fb5ecc55435e4c8ff734b04558a4c724b25c
Lextered <gahm.scema@gmail.com>
2022-02-15 11:18:04 +0530

    Fixes

--- getconf.js
+++ getconf.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
-const CONFIG_FILE_URL = process.env.CONFIG_FILE_URL || "https://file2directlink.herokuapp.com/383067522404870286405302392/rclone.conf";
+const CONFIG_FILE_URL = process.env.CONFIG_FILE_URL;
 
-exec(`curl -o --output-dir /app/.config/rclone/ --output rclone.conf ${ CONFIG_FILE_URL }`, (error, stdout, stderr) => {
+exec(`wget "${ CONFIG_FILE_URL }"`, (error, stdout, stderr) => {
     if (error) {
         console.log(`[Error] ${error.message}`);
         return;
